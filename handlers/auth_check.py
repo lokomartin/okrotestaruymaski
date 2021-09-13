d864fca35143c8496e8af2e9a7cd4b038f0e88a3
huzunluartemis <84624971+huzunluartemis@users.noreply.github.com>
2021-09-13 15:37:33 +0300

    3

--- handlers/auth_check.py
+++ handlers/auth_check.py
@@ -4,10 +4,15 @@ from configs import Config
 
 async def AuthCheck(chat_id: int, user_id: int):
     if 0 in Config.AUTH_IDS:
+        print("AuthCheck - bot is public. returning true.")
         return True
     elif user_id in Config.AUTH_IDS:
+        print("AuthCheck - userid found. returning true.")
         return True
     elif chat_id in Config.AUTH_IDS:
+        print("AuthCheck - chat_id found. returning true.")
         return True
     else:
+        print("AuthCheck - nothing found. returning false. if you did somethign wrong, read all readme")
         return False
+        
